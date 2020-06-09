25695a6ad578db52857429e086ad64a8697cb383
Josh Bright <josh.bright@gmail.com>
2020-06-08 23:39:34 -0700

    Day 3: Setup for a back buffer

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -6,10 +6,46 @@
 
 
 global_variable bool Running;
-
+global_variable BITMAPINFO BitMapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
 
 internal void Win32ResizeDIBSection(int width, int height) {
+	if (BitmapHandle) {
+		DeleteObject(BitmapHandle);
+	} else {
+		BitmapDeviceContext = CreateCompatibleDC(0);
+	}
 
+	BITMAPINFO BitMapInfo;
+	BitMapInfo.bmiHeader.biSize = sizeof(BitMapInfo.bmiHeader);
+	BitMapInfo.bmiHeader.biWidth = width;
+	BitMapInfo.bmiHeader.biHeight = height;
+	BitMapInfo.bmiHeader.biPlanes = 1;
+	BitMapInfo.bmiHeader.biBitCount = 32;
+	BitMapInfo.bmiHeader.biCompression = BI_RGB;
+	
+	BitmapDeviceContext = CreateCompatibleDC(0);
+
+	HBITMAP BitmapHandle = CreateDIBSection(
+		BitmapDeviceContext,
+		&BitMapInfo,
+		DIB_RGB_COLORS,
+		&BitmapMemory,
+		0,
+		0
+	);
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, int x, int y, int width, int height) {
+
+	StretchDIBits(
+		DeviceContext,
+		x, y, width, height,
+		x, y, width, height,
+		0, 0, DIB_RGB_COLORS, SRCCOPY
+	);
 }
 
 
@@ -17,6 +53,11 @@ LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WPara
 	LRESULT result = 0;
 	switch(Message) {
 		case WM_SIZE: {
+			RECT ClientRect;
+			GetClientRect(Window, &ClientRect);
+			int width = ClientRect.right - ClientRect.left;
+			int height = ClientRect.bottom - ClientRect.top;
+			Win32ResizeDIBSection(width, height);
 			OutputDebugStringA("WM_SIZE\n");
 		} break;
 		case WM_DESTROY: {
@@ -33,11 +74,11 @@ LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WPara
 		case WM_PAINT: {
 			PAINTSTRUCT Paint;
 			HDC DeviceContext = BeginPaint(Window, &Paint);
-			int X = Paint.rcPaint.left;
-			int Y = Paint.rcPaint.top;
-			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-			PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
+			int x = Paint.rcPaint.left;
+			int y = Paint.rcPaint.top;
+			int height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			int width = Paint.rcPaint.right - Paint.rcPaint.left;
+			Win32UpdateWindow(DeviceContext, x, y, width, height);
 			EndPaint(Window, &Paint);
 		} break;
 		default: {
