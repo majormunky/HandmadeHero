5952fb8116230b3253efa186711d69b23a3beaf0
Josh Bright <josh.bright@gmail.com>
2020-06-09 05:39:23 -0700

    Day 2: Instead of creating a message box, we now create a blank window

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,6 +1,79 @@
 #include <windows.h>
 
+
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam) {
+	LRESULT result = 0;
+	switch(Message) {
+		case WM_SIZE: {
+			OutputDebugStringA("WM_SIZE\n");
+		} break;
+		case WM_DESTROY: {
+			OutputDebugStringA("WM_DESTROY\n");
+		} break;
+		case WM_CLOSE: {
+			OutputDebugStringA("WM_CLOSE\n");
+		} break;
+		case WM_ACTIVATEAPP: {
+			OutputDebugStringA("WM_ACTIVATEAPP\n");
+		} break;
+		case WM_PAINT: {
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+			PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
+			EndPaint(Window, &Paint);
+		} break;
+		default: {
+			result = DefWindowProc(Window, Message, WParam, LParam);
+		} break;
+	}
+	return(result);
+}
+
+
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR LpCmdLine, int ncmdShow) {
-	MessageBox(0, "This is Handmade Hero", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
+	WNDCLASS WindowClass = {};
+
+	WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.hInstance = hInstance;
+	WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+	if (RegisterClass(&WindowClass)) {
+		HWND WindowHandle = CreateWindowEx(
+			0,
+			WindowClass.lpszClassName,
+			"Handmade Hero",
+			WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0,
+			0,
+			hInstance,
+			0
+		);
+		if (WindowHandle) {
+			MSG Message;
+			for (;;) {
+				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);	
+				if (MessageResult > 0) {
+					TranslateMessage(&Message);
+					DispatchMessage(&Message);
+				} else {
+					break;
+				}
+			}
+			
+
+		}
+	} else {
+
+	}
+
 	return(0);
 }
