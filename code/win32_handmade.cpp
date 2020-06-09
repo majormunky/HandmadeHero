52653521891f526ce90b20ee2eb6e7410cdda9d9
Josh Bright <josh.bright@gmail.com>
2020-06-09 05:08:30 -0700

    Updated program to show a windows message box to test functionality

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR LpCmdLine, int ncmdShow) {
+	MessageBox(0, "This is Handmade Hero", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
 	return(0);
 }
