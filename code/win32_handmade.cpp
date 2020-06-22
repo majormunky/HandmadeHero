d9d95a90787249e9a9d9fb749b4499bf8caee29b
Josh Bright <josh.bright@gmail.com>
2020-06-22 14:59:56 -0700

    Day 4: Updated windows event loop to use PeekMessage

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -144,12 +144,12 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR LpCmdLi
 			MSG Message;
 			Running = true;
 			while(Running) {
-				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);	
-				if (MessageResult > 0) {
+				while(PeekMessage(&Message, 0, 0, 0, PM_REMOVE)) {
+					if (Message.message == WM_QUIT) {
+						Running = false;	
+					}
 					TranslateMessage(&Message);
-					DispatchMessage(&Message);
-				} else {
-					break;
+					DispatchMessage(&Message);	
 				}
 			}
 		}
