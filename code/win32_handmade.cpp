299275dca5c18a11eb4c074d2c6c908f8dd206e1
Josh Bright <josh.bright@gmail.com>
2020-06-22 20:40:45 -0700

    Day 7: Updated x input loading code to try to load 1.4 first

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -50,7 +50,10 @@ global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub;
 #define XInputSetState XInputSetState_
 
 internal void Win32LoadXInput(void) {
-	HMODULE XInputLibrary = LoadLibraryA("xinput1_3.dll");
+	HMODULE XInputLibrary = LoadLibraryA("xinput1_4.dll");
+	if (!XInputLibrary) {
+		XInputLibrary = LoadLibraryA("xinput1_3.dll");	
+	}
 	if (XInputLibrary) {
 		XInputGetState = (x_input_get_state *)GetProcAddress(XInputLibrary, "XInputGetState");
 		XInputSetState = (x_input_set_state *)GetProcAddress(XInputLibrary, "XInputSetState");
